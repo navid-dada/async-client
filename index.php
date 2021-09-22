15fc6d7464e3e815f6fd75f7a2ec2649e2011df6
navid.shokri <shokri@sabacell.ir>
2021-09-22 10:31:04 +0330

    remove useless codes

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use Amp\Loop;
 use Model\AsyncClient;
 $result = 0;
 
+
 $start = microtime(true);
 $async_client  = new AsyncClient();
 $async_client->addConnection("temp","127.0.0.1", "root", "123456");
@@ -17,48 +18,3 @@ $end = microtime(true);
 echo "execution time ".($end - $start);
 
 var_dump($t);
-
-/*Loop::run(function (){
-
-
-});*/
-
-/*Loop::run(function (){
-    $start = microtime(true);
-    $dummyClient = new Model\AsyncDummy();
-    $promises = array();
-    for($i = 1 ;  $i <4; $i++){
-        array_push($promises ,Amp\call(function() use($dummyClient,$i){
-            echo "calling DoSomething $i".PHP_EOL;
-            return $dummyClient->DoSomething($i);
-        }));
-    }
-    $res = yield \Amp\Promise\all($promises);
-    //->onResolve(function ($err, $res) use($start){
-      //  echo "result" .PHP_EOL;
-      //  var_dump($err);
-      //  var_dump($res);
-      //  $end = microtime(true);
-      //  echo "execution time ".($end - $start);
-     //   return $res;
-    //});
-    var_dump($res);
-
-});*/
-
-/*$fn1 = function(){
-    echo "bega". PHP_EOL;
-    return 5;
-};
-$fn2 = function(){
-    echo "bega1". PHP_EOL;
-    return 5;
-};
-$start = microtime(true);
-Loop::Run(function () use($fn1, $fn2){
-   Loop::delay(2000, $fn1);
-   Loop::delay(1000, $fn2);
-});
-$end = microtime(true);
-echo "execution time ".($end - $start);
-echo "run finished". PHP_EOL;*/
